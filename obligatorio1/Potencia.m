ac3aaf640ed908f704e8442f30635bd9e21edd5c
goja288 <goja288@gmail.com>
2015-09-19 20:21:30 -0300

    Funcionando potencia, el vector esta normalizado

--- obligatorio1/Potencia.m
+++ obligatorio1/Potencia.m
@@ -2,50 +2,37 @@ function [autoVector,autoValor]=Potencia(A,v)
 % param A matriz
 % param v vector
 
-	%% Dados A, x0 , numiter
-	%% Desde k = 1 hasta numiter
-	%% x^k = Ax^(k−1)
-	%% Definimos αlpha^(k) como una componente de x^(k) de modulo maximo.
-	%% x^k = x^(k)/α^(k) ===> Normalizamos el vector x^(k)
-	%% Fin del bucle.
-	%% Devuelve autoValor ==> x^(iter) , alpha^(iter) .
-	
-
-	maxIteraciones = 10000;
-	iter = 0;	
-
-	tolerancia = 10^-5; 
-
-	maximo = 0;
-	indiceDelMaximo = -1;
-
-	diferencia = 1;
+	maxIteraciones = 100; % Maxima cantidad de iteraciones
+	iter = 0; % Numero de iteracion	
+	tolerancia = 10^-5; % Tolerancia
+ 
+	diferencia = 1; % Variable auxiliar para comprar con la tolerancia
 
 	autoValor = 0;
 	autoValorAnterior = -1;
 
-	x = v(:); % Lo paso a columna
+	x = v(:); % Paso el vector x0 como columna
+	y = x / norm(x); % Normalizo
 
-	% Si la cantidad de iteraciones no supero el maximo y la diferncia
+	% Si la cantidad de iteraciones no supero el maximo y la diferencia
 	% dos valores consecutivos es mayor a la tolerancia
 	while (iter < maxIteraciones && diferencia > tolerancia) 
-		x = A*x; % x^(k) = Ax^(k-1)	
-
-		[maximo,indiceDelMaximo] = max(abs(x)); % Tomo el maximo en valor absoluto del autovector
-
-		autoValor = x(indiceDelMaximo);
-
-		x = 1 / (autoValor * x); % Normalizo
-
-
+		
+		x = A*y; % x^(k) = Ax^(k-1)
+		y = x / norm(x);
+		
+		autoValor = x.'*y; % Calculo el autovector - con .' traspongo
+		
 		diferencia = abs(autoValor-autoValorAnterior);
-		autoValorAnterior = autoValor;
+		autoValorAnterior = autoValor; % lo guardo para la proxima iteracion
 
-		iter++;
+		iter++; 
 	
 	end
-	autoVector = x;
-
+	
+	
+	autoVector = y; % Normalizado
+%	autoVector = x
 end
 
 
