fe9a9052009400454a153d41b6ec7d86c41cea91
raul <raul@mgcoders.uy>
2015-09-24 23:05:43 -0300

    diagonal saco los print

--- obligatorio1/Comparacion.m
+++ obligatorio1/Comparacion.m
@@ -2,13 +2,16 @@ function Comparacion()
 
 	addpath("matrices");
 	load("matrices/harvard500.mat");
-	HARV = G;
+	HARV = G - diag(diag(G));
 	load("matrices/wb-cs-stanford.mat");
-	STANFORD = Problem.A; 
+	STANFORD = Problem.A - diag(diag(Problem.A)); 
 	X = createMatrix(50);
+	X = X - diag(diag(X)); 
+	Y = createMatrix(500);
+	Y = Y - diag(diag(Y));
 
 	disp("POTENCIA vs ARNOLDI\n");
-	set(0, 'defaultfigurevisible', 'off');
+	#set(0, 'defaultfigurevisible', 'off');
 
 	maxIteraciones = 20;
 	tolerancia = 10^-20;
@@ -19,20 +22,41 @@ function Comparacion()
 	[autoVector,autoValor,errores,iter] = Potencia(X, maxIteraciones,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion X;");
-	print('ErrorVsIteracionPotencia-X','-dpng');
+	#plot(errores,";Error vs Iteracion X;");
+	#print('ErrorVsIteracionPotencia-X','-dpng');
 
 	disp("ARNOLDI X: ");
 	[autoVector,autoValor,errores,iter] = Sistema(X, maxIteraciones,alpha,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion X;");
-	print('ErrorVsIteracionArnoldi-X','-dpng');
+	#plot(errores,";Error vs Iteracion X;");
+	#print('ErrorVsIteracionArnoldi-X','-dpng');
 
 	disp("PAGERANKPOW X: ");
 	[autoVector,iter] = pagerankpow(X);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d \n\n\n',probabilidad,indice,iter);
+
+	disp("################### Y ########################\n\n");
+	disp("POTENCIA Y: ");
+	[autoVector,autoValor,errores,iter] = Potencia(Y, maxIteraciones,tolerancia);
+	[probabilidad indice] = max(autoVector);
+	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
+	#plot(errores,";Error vs Iteracion Y;");
+	#print('ErrorVsIteracionPotencia-Y','-dpng');
+
+	disp("ARNOLDI Y: ");
+	[autoVector,autoValor,errores,iter] = Sistema(Y, maxIteraciones,alpha,tolerancia);
+	[probabilidad indice] = max(autoVector);
+	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
+	#plot(errores,";Error vs Iteracion Y;");
+	#print('ErrorVsIteracionArnoldi-Y','-dpng');
+
+	disp("PAGERANKPOW Y: ");
+	[autoVector,iter] = pagerankpow(Y);
+	[probabilidad indice] = max(autoVector);
+	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d \n\n\n',probabilidad,indice,iter);
+
 
 
 	disp("################### HARVARD ########################\n\n");
@@ -40,15 +64,15 @@ function Comparacion()
 	[autoVector,autoValor,errores,iter] = Potencia(HARV, maxIteraciones,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion POTENCIA;");
-	print('ErrorVsIteracionPotencia-Harvard','-dpng');
+	#plot(errores,";Error vs Iteracion POTENCIA;");
+	#print('ErrorVsIteracionPotencia-Harvard','-dpng');
 
 	disp("ARNOLDI HARVARD: ");
 	[autoVector,autoValor,errores,iter] = Sistema(HARV, maxIteraciones,alpha,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion ARNOLDI;");
-	print('ErrorVsIteracionArnoldi-Harvard','-dpng');
+	#plot(errores,";Error vs Iteracion ARNOLDI;");
+	#print('ErrorVsIteracionArnoldi-Harvard','-dpng');
 
 	disp("PAGERANKPOW HARVARD: ");
 	[autoVector,iter] = pagerankpow(HARV);
@@ -61,27 +85,22 @@ function Comparacion()
 	[autoVector,autoValor,errores,iter] = Potencia(STANFORD, maxIteraciones,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion POTENCIA;");
-	print('ErrorVsIteracionPotencia-Stanford','-dpng');
+	#plot(errores,";Error vs Iteracion POTENCIA;");
+	#print('ErrorVsIteracionPotencia-Stanford','-dpng');
 
 
 	disp("ARNOLDI STANFORD: ");
 	[autoVector,autoValor,errores,iter] = Sistema(STANFORD, maxIteraciones,alpha,tolerancia);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d AutoValor= %g \n',probabilidad,indice,iter,autoValor);
-	plot(errores,";Error vs Iteracion ARNOLDI;");
-	print('ErrorVsIteracionArnoldi-Stanford','-dpng');
+	#plot(errores,";Error vs Iteracion ARNOLDI;");
+	#print('ErrorVsIteracionArnoldi-Stanford','-dpng');
 
 	disp("PAGERANKPOW STANFORD: ");
 	[autoVector,iter] = pagerankpow(STANFORD);
 	[probabilidad indice] = max(autoVector);
 	fprintf(1,'Probabilidad: %g Indice= %d Iteracion= %d \n\n\n',probabilidad,indice,iter);
 
-
-	
-
-	#plot(erroresPotencias,";errores Potencias;");
-	#plot(erroresSistema,";errores Sistemas;");
 endfunction
 
 #crear matriz esparsa de dim*dim
@@ -104,3 +123,5 @@ function [A] = createMatrix(dim)
 		end
 	end
 endfunction
+
+
