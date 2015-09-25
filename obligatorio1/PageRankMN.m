22cd6056b7510ec20becf7b9b2ab536c9bc33dd4
goja288 <goja288@gmail.com>
2015-09-25 00:12:31 -0300

    Cambie el gauss seidel y otros detalles

--- obligatorio1/PageRankMN.m
+++ obligatorio1/PageRankMN.m
@@ -3,15 +3,16 @@ function PageRankMN()
 	p = 0.85; % Probabilidad p
 
 	load matrices/harvard500.mat
-	G = G'- diag(diag(G));
+	G = G - diag(diag(G));
 	
 	%G = [0 0 1 0; 1 0 1 0; 0 1 0 0; 1 0 1 0];
-	
+	%G = [1 -1 4; 3 2 -1; 2 1 -1];
+	%G =  [0 0 0 1 0 1; 1 0 0 0 0 0; 0 1 0 0 0 0; 0 1 1 0 0 0; 0 0 1 0 0 0; 1 0 1 0 0 0]
 	
 	[n,n] = size(G);
 	
-	I = eye(n); % creo la matriz identidad
-	
+	%I = speye(n,n); % creo la matriz identidad como esparsa
+	I = eye(n,n); % creo la matriz identidad
 	
 	% Quiero obtener la matriz D
 	D = zeros(n,n);
@@ -19,88 +20,68 @@ function PageRankMN()
     	L{j} = find(G(:,j));
     	c(j) = length(L{j}); % aca obtengo cuantos unos hay en la columna
 		if (c(j) > 0)
-			D(j,j) = 1/n; 
+			D(j,j) = 1/c(j); 
 		end
 	end	
 	
 	
 	
+	A = (I - p*(G*D));
 	
-	A = (I - p*G*D);
 	b = ones(n,1);
 	
-	%A
-	%b
-	%D
-	%c
-	%I
 	
-	gauss_seidel(A,b,100);
+	x = GaussSeidel(A,b);
+	x = x/sum(x);
+	sum(x)
+	max(x)
 	
 endfunction
 
 %%%% 
 
-function gauss_seidel(A, b, N)
+function x = GaussSeidel(A,b,es,maxit)
 
-	%Gauss_seidel(A, b, N) solve iteratively a system of linear equations 
-	%whereby A is the coefficient matrix, and b is the right-hand side column vector.
-	%N is the maximum number of iterations.
-	%The method implemented is the Gauss-Seidel iterative. 
-	%The starting vector is the null vector, but can be adjusted to one's needs.
-	%The iterative form is based on the Gauss-Seidel transition/iteration matrix
-	%Tg = inv(D-L)*U and the constant vector cg = inv(D-L)*b.
-	%The output is the solution vector x.
+	% x = GaussSeidel(A,b):
+	%   Gauss Seidel method.
+	% input:
+	%   A = coefficient matrix
+	%   b = right hand side vector
+	%   es = (optional) stop criterion (%) (default = 0.00001)
+	%   maxit = (optional) max iterations (default = 50)
+	% output:
+	%   x = solution vector
 
-	%This file follows the algorithmic guidelines given in the book 
-	%Numerical Analysis, 7th Ed, by Burden & Faires
-
-	%Author: Alain G. Kapitho
-	%Date  : Dec. 2005
-	%Rev.  : Aug. 2007
-
-
-	n = size(A,1);
-	%splitting matrix A into the three matrices L, U and D
-	D = diag(diag(A));
-	L = tril(-A,-1);
-	U = triu(-A,1);
-
-	%transition matrix and constant vector used for iterations
-	Tg = inv(D-L)*U; 
-	cg = inv(D-L)*b;
-
-	tol = 1e-10;
-	k = 1;
-	x = zeros(n,1);			%starting vector
-	x(1) = 1;
-
-	while k <= N
-	   x(:,k+1) = Tg*x(:,k) + cg;
-	   if norm(x(:,k+1)-x(:,k)) < tol
-		  disp('The procedure was successful')
-		  disp('Condition ||x^(k+1) - x^(k)|| < tol was met after k iterations')
-		  disp(k); 
-		  disp('x = ');
-		  %disp(x(:,k+1));
-		  sum(x(:,k+1))
-		  
-		  y = x(:,k+1) / norm(x(:,k+1));
-		  sum(y)
-		  max(y)
-		  
-		  break
-	   end
-	   k = k+1;
+	if nargin<4, maxit=50; end
+	if nargin<3, es=0.00001; end
+	[m,n] = size(A);
+	if m~=n, error('Matrix A must be square'); end
+	C = A;
+	for i = 1:n
+		C(i,i) = 0;
+		x(i) = 0;
+	end
+	x = x';
+	for i = 1:n
+		C(i,1:n) = C(i,1:n)/A(i,i);
+	end
+	for i = 1:n
+		d(i) = b(i)/A(i,i);
 	end
 
-	if norm(x(:,k+1)- x(:,k)) > tol || k > N
-	   disp('Maximum number of iterations reached without satisfying condition:')
-	   disp('||x^(k+1) - x^(k)|| < tol'); disp(tol);
-	   disp('Please, examine the sequence of iterates')
-	   disp('In case you observe convergence, then increase the maximum number of iterations')
-	   disp('In case of divergence, the matrix may not be diagonally dominant')
-	   disp(x');
-	   
+	iter = 0;
+	while (1)
+	  xold = x;
+	  for i = 1:n
+		x(i) = d(i)-C(i,:)*x;
+		if x(i) ~= 0
+		  ea(i) = abs((x(i) - xold(i))/x(i)) * 100;
+		end
+	  end
+	  iter = iter+1;
+	  if max(ea)<=es | iter >= maxit,
+		 break,
+	  end
 	end
 endfunction
+
