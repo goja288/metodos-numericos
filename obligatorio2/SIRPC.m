7d2f41b1d7c6eab595db2023ed9075bb0e68ba0c
goja288 <goja288@gmail.com>
2015-10-18 10:24:16 -0300

    Obligatorio 2

---
+++ obligatorio2/SIRPC.m
@@ -0,0 +1,15 @@
+%-----------------------------
+% Metodo Predictor Correctos 
+%-----------------------------
+
+% gamma -> tasa de recuperacion
+% beta -> tasa de contagio
+% S -> poblacion inicial de susceptibles
+% I -> poblacion inicial de infectados
+% R -> poblacion inicial de removidos
+% h -> paso 
+function [res, errorGlobal] = SIRPC(gamma,beta,S,I,R,h)
+
+
+
+end
